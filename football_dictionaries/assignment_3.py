e903a9fdcfeb411d1fb29d3508d8bad498e8c9d3
Alia AlGhamdi <82Luli02@gmail.com>
2024-08-23 19:57:18 +0300

    Completed the implementation of players_by_country_and_position in assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,16 @@
 def players_by_country_and_position(squads_list):
-    pass
+    group_players_by_country_and_position_dict = {}
+
+    for player in squads_list:
+        country = player['country']
+        position = player['position']
+
+        if country not in group_players_by_country_and_position_dict:
+            group_players_by_country_and_position_dict[country] = {}
+
+        if position not in group_players_by_country_and_position_dict[country]:
+            group_players_by_country_and_position_dict[country][position] = []
+
+        group_players_by_country_and_position_dict[country][position].append(player)
+
+    return group_players_by_country_and_position_dict
