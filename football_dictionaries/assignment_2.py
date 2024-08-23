3fe41ed71767b3347165a8f56c9cecd04cc75e50
Alia AlGhamdi <82Luli02@gmail.com>
2024-08-23 19:55:33 +0300

    Completed the implementation of players_by_position in assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,12 @@
 def players_by_position(squads_list):
-    pass
+  group_players_by_position_dict={}
+    
+  for player in squads_list:
+    position = player['position']
+
+    if position not in group_players_by_position_dict:
+      group_players_by_position_dict[position]=[]
+
+    group_players_by_position_dict[position].append(player)
+
+  return group_players_by_position_dict
